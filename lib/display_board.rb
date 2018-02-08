3f06aace5ab70ea25cdf15dfc7af2d50b1f02a41
Wendy Huang <wendy.huang@wework.com>
2018-02-08 06:37:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,9 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", ]
 
 def display_board
   puts "   |   |   "
-  puts "---------"
+  puts "-----------"
   puts "   |   |   "
-  puts "---------"
+  puts "-----------"
   puts "   |   |   "
 end
 
